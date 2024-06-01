cf73d2fd91ac5b34b1a503942111f8d952908309
maksim <suslovsms@yandex.ru>
2024-06-01 16:38:15 +0300

    First commit!

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Всем привет! Меня зовут Максим, в текущий момент являюсь Frontend QA в компани TradingView. \n
+          Хочу освоить фронтенд разработку для написания собственных проектов. \n
+          TS, React \n
+          Pytest, Selenium, XCUITest \n
+          Ручной тестировщик, который все время что то автоматизирует. Настало время заняться собсвенными разработками.
         </p>
       </header>
     </div>
